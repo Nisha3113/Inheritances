9cd26455d39ac6b77ca56964548475cb62985c06
Batch1 <Batch1@VITUNIVERSITY.LOCAL>
2026-04-16 16:52:32 +0530

    Initial commit with Inheritance logic

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -3,6 +3,7 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
         Animal myDog = new Dog(); 
+        // We are calling .sound() which now exists in Animal.java
         System.out.println("Inheritance Test: " + myDog.sound());
     }
 }
